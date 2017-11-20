909e011f2dc4776060fe552c3eb015bcb0c74926
Chris Gullickson <cgullickson5@gmail.com>
2017-11-20 23:54:36 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown (num)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (num)
+    while num > 0
+    puts "#{num} SECOND(S)!"
+    sleep(1)
+    num -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
